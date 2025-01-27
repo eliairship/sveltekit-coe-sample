95624d0c4922ac8c7784744c70fe7093387bef47
Matt Koehler <matt.koehler@unosquare.com>
2025-01-27 12:20:44 -0600

    removing old svelte 4 stuff
    Co-authored-by: Steven Walker <swalk001@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,9 @@
 
   let { data }: { data: PageData } = $props();
 
+	import { onMount } from 'svelte';
 	import { getCounterContext } from "./counterContext";
-  import { writable, derived } from 'svelte/store';
-  import {onMount} from 'svelte'
 
-  let countStore = writable(0);
   let countState = $state(0);
   let value = getCounterContext()
   let derivedValue = $derived(countState * 2)
@@ -16,7 +14,6 @@
   let endpoint = "https://jsonplaceholder.typicode.com/posts"
   onMount(async () => {
     const response = await fetch(endpoint);
-    console.log('🧙‍♂️ where are you')
     const data = await response.json();
     posts = data;
   })
@@ -25,15 +22,11 @@
 
 <h1 class='text-2xl text-red-700'>Welcome to SvelteKittted</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>Count Store: {$countStore}</p>
 <p>Derived State: {derivedValue}</p>
 <br />
 <br />
 <h1>{data.post.title}</h1>
 <div>{@html data.post.content}</div>
-<br />
-
-<button onclick={() => countStore.update((n) => n + 1)}>increment countStore {$countStore}</button>
 <br />
 <button onclick={() => countState++}>increment CountState {countState}</button>
 <br />
